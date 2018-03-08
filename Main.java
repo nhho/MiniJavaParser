742c608faceccb048701d742c9c8d01ec916d36a
Anson Ho <mrho888@gmail.com>
2018-03-08 13:02:15 +0800

    Initialize

---
+++ Main.java
@@ -0,0 +1,15 @@
+import syntaxtree.*;
+import visitor.*;
+import myparser.*;
+
+public class Main {
+   public static void main(String [] args) {
+      try {
+         Program root = new MiniJavaParser(System.in).Goal();
+          root.accept(new PrettyPrintVisitor());
+      }
+      catch (ParseException e) {
+         System.out.println(e.toString());
+      }
+   }
+}
